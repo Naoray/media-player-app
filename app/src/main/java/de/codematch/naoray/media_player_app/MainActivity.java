dbbef43132ded441ab3b52a02622f8f06093935f
Schlaubi <nicoweber01@aol.com>
2015-11-02 16:05:01 +0100

    -MainMenue zentriert -MenueItems starten entsprechend neue Activity beim Onclick

--- app/src/main/java/de/codematch/naoray/media_player_app/MainActivity.java
+++ app/src/main/java/de/codematch/naoray/media_player_app/MainActivity.java
@@ -1,17 +1,17 @@
 package de.codematch.naoray.media_player_app;
 
-import android.content.Context;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private String[] menuItems = {"Live Stream", "Mediathek", "Credits"};
+    private Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +19,20 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         ListView menuView = (ListView) findViewById(R.id.menu_list);
-        ArrayAdapter<String> menuViewAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, menuItems);
+        ArrayAdapter<String> menuViewAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, menuItems);
         menuView.setAdapter(menuViewAdapter);
         menuView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Toast.makeText(MainActivity.this, parent + " - " + id, Toast.LENGTH_SHORT).show();
+                if (id == 0) {
+                    intent = new Intent(MainActivity.this, LiveStreamActivity.class);
+                } else if (id == 1) {
+                    intent = new Intent(MainActivity.this, MediathekActivity.class);
+                } else if (id == 2) {
+                    intent = new Intent(MainActivity.this, CreditsActivity.class);
+                }
+                startActivity(intent);
+                //Toast.makeText(MainActivity.this, parent + " - " + id, Toast.LENGTH_SHORT).show();
             }
         });
     }
