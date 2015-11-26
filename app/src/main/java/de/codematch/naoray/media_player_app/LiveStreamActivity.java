b2bf5276f96991e86e112c71792bea492cf49ea3
Schlaubi <nicoweber01@aol.com>
2015-11-26 16:37:30 +0100

    -VideoPlayer in die App integriert -ActionBar der LiveStreamActivity wird ausgeblendet (In der AndroidManifest festgelegt) -Hintergrundfarbe der LiveStreamActivity auf schwarz geändert, damit das Video kein weißen Rand aufweist -StatusBar wird ausgeblendet und erst beim "Reinwischen" wieder sichbar, bis sie dann wieder verschwindet -NavigationBar bleibt die ganze Zeit über sichtbar  ---> Code für den Immersive-Mode vorhanden, aber die NavigationBar überdeckt bei Erscheinen dann die Steuerelemente des Videos und macht bspw. ein Vorspulen unmöglich

--- app/src/main/java/de/codematch/naoray/media_player_app/LiveStreamActivity.java
+++ app/src/main/java/de/codematch/naoray/media_player_app/LiveStreamActivity.java
@@ -1,13 +1,91 @@
 package de.codematch.naoray.media_player_app;
 
+import android.app.ProgressDialog;
+import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.WindowManager;
+import android.widget.MediaController;
+import android.widget.VideoView;
 
 public class LiveStreamActivity extends AppCompatActivity {
+
+
+    // Declare variables
+    ProgressDialog pDialog;
+    VideoView videoview;
+    // Insert your Video URL
+    String VideoURL = "http://regiotainment.mni.thm.de:3000/videostorage/playliststorage/5623ca95e6cc3b74106e1bba/mainpanel/streams.m3u8";
+    //"http://regiotainment.mni.thm.de/MobileVideos/Regiotainment.mp4";
+    //"http://www.androidbegin.com/tutorial/AndroidCommercial.3gp";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        // Get the layout from video_main.xml
         setContentView(R.layout.activity_live_stream);
+
+
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        // Find your VideoView in your video_main.xml layout
+        videoview = (VideoView) findViewById(R.id.VideoView);
+        // Execute StreamVideo AsyncTask
+        // Create a progressbar
+        pDialog = new ProgressDialog(LiveStreamActivity.this);
+        // Set progressbar title
+        pDialog.setTitle("Android Video Streaming Tutorial");
+        // Set progressbar message
+        pDialog.setMessage("Video is Buffering...");
+        pDialog.setIndeterminate(false);
+        pDialog.setCancelable(false);
+        // Show progressbar
+        pDialog.show();
+
+        try {
+            // Start the MediaController
+            MediaController mediacontroller = new MediaController(
+                    LiveStreamActivity.this);
+            mediacontroller.setAnchorView(videoview);
+            // Get the URL from String VideoURL
+            Uri video = Uri.parse(VideoURL);
+            videoview.setMediaController(mediacontroller);
+            videoview.setVideoURI(video);
+
+        } catch (Exception e) {
+            Log.e("Error", e.getMessage());
+            e.printStackTrace();
+        }
+
+        videoview.requestFocus();
+        videoview.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+            // Close the progress bar and play the video
+            public void onPrepared(MediaPlayer mp) {
+                pDialog.dismiss();
+                videoview.start();
+            }
+        });
+
     }
+
+    //Notification- und Navigation-Leiste werden ausgeblendet. Die Activity geht somit quasi in einen Vollbild-Modus
+    //Wischt der User aus Notification- bzw. Navigation-Leiste, so werden diese für einen kurzen Moment wieder eingeblendet
+    //LEIDER WERDEN HIER DIE STEUERUNGSELEMENTE DES VIDEOS ÜBERDECKT!!
+  /*  @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            View decorView = this.getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                            | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                            | View.SYSTEM_UI_FLAG_FULLSCREEN
+                            | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY);}
+    }*/
 }
+
