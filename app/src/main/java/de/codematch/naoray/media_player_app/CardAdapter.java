bea99fc5bc5d3a1511d474f4a9c2426ee538eb56
Katja24 <katja.kubitta@online.de>
2016-01-14 09:44:16 +0100

    PopUp für Card erstellt.

--- app/src/main/java/de/codematch/naoray/media_player_app/CardAdapter.java
+++ app/src/main/java/de/codematch/naoray/media_player_app/CardAdapter.java
@@ -1,10 +1,12 @@
 package de.codematch.naoray.media_player_app;
 
+import android.graphics.Color;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -14,9 +16,8 @@ import java.util.ArrayList;
  * Created on 15.12.2015.
  */
 public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder> {
-    TextView descriptionMain;
+
     private ArrayList<Card> cardArrayList;
-    //    private ClickListener clickListener;
     private MainMenuActivity main;
 
     public CardAdapter(ArrayList<Card> cardArrayList, MainMenuActivity main) {
@@ -35,11 +36,11 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
     public void onBindViewHolder(CardViewHolder holder, int position) {
         holder.titel.setText(cardArrayList.get(position).getTitel());
         holder.description.setText(cardArrayList.get(position).getDescription());
-        //holder.image.setImageResource(cardArrayList.get(position).getImage());
+        //Hintergrundbild nur dan setzen, wenn es eins gibt.
         if (cardArrayList.get(position).getImage() != 0)
             holder.image.setImageResource(cardArrayList.get(position).getImage());
-        else
-            holder.test1.setCardBackgroundColor(cardArrayList.get(position).getBackground());
+        // Hintergrundfarbe setzen
+        holder.background.setCardBackgroundColor(Color.parseColor(cardArrayList.get(position).getBackground()));
 
     }
 
@@ -48,15 +49,11 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
         return cardArrayList.size();
     }
 
-    /*
-        public void setClickListener(ClickListener clickListener){
-            this.clickListener = clickListener;
-        }
-    */
     public class CardViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         TextView titel, description;
         ImageView image;
-        CardView test1;
+        CardView background;
+        ImageButton dots;
 
         public CardViewHolder(View itemView) {
             super(itemView);
@@ -64,14 +61,30 @@ public class CardAdapter extends RecyclerView.Adapter<CardAdapter.CardViewHolder
             titel = (TextView) itemView.findViewById(R.id.title);
             description = (TextView) itemView.findViewById(R.id.description);
             image = (ImageView) itemView.findViewById(R.id.thumbnail);
-            test1 = (CardView) itemView.findViewById(R.id.card_view);
+            background = (CardView) itemView.findViewById(R.id.card_view);
+            dots = (ImageButton) itemView.findViewById(R.id.threeDots);
 
             itemView.setOnClickListener(this);
+
+            /**
+             * ClickListener für die drei Punkte, oben rechts einer Karte
+             * Diese ruft eine Methode der MainActvity auf, da dort der OnClick gehandelt werden soll
+             */
+            dots.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    main.cardDescription(getAdapterPosition());
+                }
+            });
         }
 
+        /**
+         * ClickListener für die Karte selbst.
+         * Diese ruft eine Methode der MainActvity auf, da dort der OnClick gehandelt werden soll
+         */
         @Override
         public void onClick(View v) {
-            main.onClick(getAdapterPosition(), cardArrayList.get(getAdapterPosition()).getTitel());
+            main.onClick(getAdapterPosition());
         }
     }
 }
---
+++ app/src/main/java/de/codematch/naoray/media_player_app/CardDescriptionActivity.java
@@ -0,0 +1,56 @@
+package de.codematch.naoray.media_player_app;
+
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+import android.widget.TextView;
+
+public class CardDescriptionActivity extends AppCompatActivity {
+
+    String titel;
+    String largeDescription;
+    String color;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_card_description);
+
+        titel = getIntent().getStringExtra("CardTitle");
+        largeDescription = getIntent().getStringExtra("CardLargeDescription");
+        color = getIntent().getStringExtra("CardColor");
+
+        // Titel setzen
+        this.setTitle(titel);
+
+        // Text setzen
+        TextView text = (TextView) findViewById(R.id.largeText);
+        text.setText(largeDescription);
+
+        // Farbe der Titelbar setzen
+        getSupportActionBar().setBackgroundDrawable(new ColorDrawable(Color.parseColor(color)));
+
+
+        // Farbe der Stausbar setzen
+        // Offen: das die Stautusbar einen Tick dunkler ist
+        Window window = this.getWindow();
+        window.setStatusBarColor(darker(Color.parseColor(color), 0.001f));
+    }
+
+    /**
+     * Returns darker version of specified <code>color</code>.
+     */
+    public int darker(int color, float factor) {
+        int a = Color.alpha(color);
+        int r = Color.red(color);
+        int g = Color.green(color);
+        int b = Color.blue(color);
+
+        return Color.argb(a,
+                Math.max((int) (r * factor), 0),
+                Math.max((int) (g * factor), 0),
+                Math.max((int) (b * factor), 0));
+    }
+}
