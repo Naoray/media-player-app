058340af6b95c1fbbf9ab2382cf66214488d4f2a
MLB93 <mlb1993@outlook.com>
2016-01-21 14:09:46 +0100

    Kommentare zur Klasse AES hinzugefügt

--- app/src/main/java/de/codematch/naoray/media_player_app/AES.java
+++ app/src/main/java/de/codematch/naoray/media_player_app/AES.java
@@ -9,6 +9,9 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * The Class AES contains Methods to en- & decrypt Strings with AESAlgorithm
+ */
 public class AES {
 
     //Needs to be the same key in the php-file
@@ -30,21 +33,37 @@ public class AES {
         }
     }*/
 
+    /**
+     * Encrypt a String with AES-Algorithm
+     *
+     * @param text
+     * @return String
+     * @throws GeneralSecurityException
+     * @throws UnsupportedEncodingException
+     */
     public static String encrypt(String text)
             throws GeneralSecurityException, UnsupportedEncodingException {
 
 
+        //Instance to encrypt
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        //Check length of the password
         byte[] keyBytes = new byte[16];
         byte[] b = key.getBytes("UTF-8");
         int len = b.length;
         if (len > keyBytes.length)
             len = keyBytes.length;
         System.arraycopy(b, 0, keyBytes, 0, len);
+
+        //Generate key
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        //Generate initialization vector
         IvParameterSpec ivSpec = new IvParameterSpec(keyBytes);
+        //initialize cipher instance
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 
+        //Encrypt
         byte[] results = cipher.doFinal(text.getBytes("ISO-8859-1"));
         return Base64.encodeToString(results, Base64.CRLF); //Base64.CRLF is very important here
 
@@ -62,20 +81,36 @@ public class AES {
         return s;*/
     }
 
+    /**
+     * Decrypt a String with AES-Algorithm
+     *
+     * @param text
+     * @return String
+     * @throws GeneralSecurityException
+     * @throws UnsupportedEncodingException
+     */
     public static String decrypt(String text)
             throws GeneralSecurityException, UnsupportedEncodingException {
 
+        //Instance to decrypt
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        //Check length of the password
         byte[] keyBytes = new byte[16];
         byte[] b = key.getBytes("UTF-8");
         int len = b.length;
         if (len > keyBytes.length)
             len = keyBytes.length;
         System.arraycopy(b, 0, keyBytes, 0, len);
+
+        //Generate key
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        //Generate initialization vector
         IvParameterSpec ivSpec = new IvParameterSpec(keyBytes);
+        //initialize cipher instance
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
 
+        //Decrypt
         byte[] results = cipher.doFinal(Base64.decode(text, Base64.DEFAULT));
         return new String(results, "UTF-8");
 
