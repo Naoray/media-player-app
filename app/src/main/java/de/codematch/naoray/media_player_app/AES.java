b895eab05f7a7d9829d9060b6614b0404714556b
Schlaubi <nicoweber01@aol.com>
2016-02-15 14:50:52 +0100

    - Kommentare für AES und Volley wurde manuell hinzugefügt, da beheben des Merge-Konflikt nicht geklappt hat

--- app/src/main/java/de/codematch/naoray/media_player_app/AES.java
+++ app/src/main/java/de/codematch/naoray/media_player_app/AES.java
@@ -9,6 +9,9 @@ import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
+/**
+ * The Class AES contains Methods to en- & decrypt Strings with AESAlgorithm
+ */
 public class AES {
 
     //Needs to be the same key in the php-file
@@ -16,53 +19,66 @@ public class AES {
     protected static final String key = "1234567890123456";
 
     /**
-     * Encrypts the text with AES/CBC/PKCS5Padding
-     *
-     * @param text for encryption
-     * @return encrypted String
+     * Encrypt a String with AES-Algorithm
+     * @param text Text, which has to be encypted
+     * @return String
      * @throws GeneralSecurityException
      * @throws UnsupportedEncodingException
      */
     public static String encrypt(String text) throws GeneralSecurityException, UnsupportedEncodingException {
 
+        //Instance to encrypt
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        //Check length of the password
         byte[] keyBytes = new byte[16];
         byte[] b = key.getBytes("UTF-8");
         int len = b.length;
         if (len > keyBytes.length)
             len = keyBytes.length;
         System.arraycopy(b, 0, keyBytes, 0, len);
+
+        //Generate key
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        //Generate initialization vector
         IvParameterSpec ivSpec = new IvParameterSpec(keyBytes);
+        //initialize cipher instance
         cipher.init(Cipher.ENCRYPT_MODE, keySpec, ivSpec);
 
+        //Encrypt
         byte[] results = cipher.doFinal(text.getBytes("ISO-8859-1"));
         return Base64.encodeToString(results, Base64.CRLF); //Base64.CRLF is very important here
     }
 
     /**
-     * Decrypts the text with AES/CBC/PKCS5Padding
-     *
-     * @param text for decryption
-     * @return decrypted String
+     * Decrypt a String with AES-Algorithm
+     * @param text Text, which has to be decrypted
+     * @return String
      * @throws GeneralSecurityException
      * @throws UnsupportedEncodingException
-     * @Info: currently not used
      */
     public static String decrypt(String text)
             throws GeneralSecurityException, UnsupportedEncodingException {
 
+        //Instance to decrypt
         Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
+
+        //Check length of the password
         byte[] keyBytes = new byte[16];
         byte[] b = key.getBytes("UTF-8");
         int len = b.length;
         if (len > keyBytes.length)
             len = keyBytes.length;
         System.arraycopy(b, 0, keyBytes, 0, len);
+
+        //Generate key
         SecretKeySpec keySpec = new SecretKeySpec(keyBytes, "AES");
+        //Generate initialization vector
         IvParameterSpec ivSpec = new IvParameterSpec(keyBytes);
+        //initialize cipher instance
         cipher.init(Cipher.DECRYPT_MODE, keySpec, ivSpec);
 
+        //Decrypt
         byte[] results = cipher.doFinal(Base64.decode(text, Base64.DEFAULT));
         return new String(results, "UTF-8");
     }
